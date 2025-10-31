fb889a2c2e2170c10d166cb4ebe1e2d2238924f7
mondblad <mondblad@gmail.com>
2025-11-01 01:27:53 +0300

    Настройка докера для общения с контейнерами

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import { OpenAPI as AuthOpenAPI } from './generate/auth-service'; 
 import { OpenAPI as UserOpenAPI } from './generate/user-service'; 
 
-AuthOpenAPI.BASE = import.meta.env.VITE_AUTH_API;
-UserOpenAPI.BASE = import.meta.env.VITE_USER_API;
+AuthOpenAPI.BASE = import.meta.env.VITE_AUTH_API || '/api/auth/';
+UserOpenAPI.BASE = import.meta.env.VITE_USER_API || '/api/user/';
 
 export default { AuthOpenAPI, UserOpenAPI };
